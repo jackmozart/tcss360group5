e39453da8bc87164994bb5647a276fa0e2f47d5a
phillip.bernard1@gmail.com <phillip.bernard1@gmail.com@338336da-c548-6115-24be-3890e3b90027>
2011-05-12 05:27:32 +0000

    Began work on ScheduleMain test class. Did some initial coding for AuthenticatableUser which resulted in coding all the child classes too. Added JUnit to build path
    --Phillip

--- trunk/src/tests/ScheduleMainTest.java
+++ trunk/src/tests/ScheduleMainTest.java
@@ -1,5 +1,21 @@
 package tests;
 
-public class ScheduleMainTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import sourcepac.ScheduleMain;
+
+public class ScheduleMainTest {
+	private ScheduleMain my_scheduler;
+	
+	@Before
+	public void startUp() {
+		my_scheduler = new ScheduleMain() ;
+	}
+	
+	@Test
+	public void loadFileTest() {
+		
+	}
+	
 }
--- trunk/src/users/Advisor.java
+++ trunk/src/users/Advisor.java
@@ -1,5 +1,29 @@
 package users;
 
+import java.util.Set;
+
+import sourcepac.Course;
+/**
+ * This class holds the information about and advisor and the suggestions for 
+ * courses that the advisor has.
+ * 
+ * @author Phillip Bernard
+ * @version 5/10/2011
+ */
 public class Advisor extends Voter {
+	
+	/**
+	 * This constructor creates an Advisor object with the given parameters
+	 * 
+	 * @param the_username is the users login ID
+	 * @param the_password is the phrase that authenticates the user
+	 * @param the_name is the user's real name
+	 * @param the_courses the courses the teacher prefers
+	 */
+	public Advisor(String the_username, String the_password, String the_name,
+			Set<Course> the_courses) {
+		super(the_username, the_password, the_name, the_courses);
+		
+	}
 
 }
