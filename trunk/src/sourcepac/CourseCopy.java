c35bf0877c61bef4b2e685abd085287abd53b974
fromtheofficeofthepresident@gmail.com <fromtheofficeofthepresident@gmail.com@338336da-c548-6115-24be-3890e3b90027>
2011-05-16 21:43:36 +0000

    course copy was updated

--- trunk/src/sourcepac/CourseCopy.java
+++ trunk/src/sourcepac/CourseCopy.java
@@ -1,10 +1,39 @@
 
 package sourcepac;
 
-public class CourseCopy {
+public class CourseCopy extends Course{
 
-  public CourseCopy(String course, String instructor, int start, int end) {
-    // TODO Auto-generated constructor stub
+  private static final String NO_TEACHER_ASSIGNED_MESSAGE = "No Teacher assigned";
+  
+  /**
+   * Teacher of course.
+   */
+  private String my_teacher;
+  
+  /**
+   * Time of the course.
+   */
+  private int my_time;
+  
+  /**
+   * This constructor creates a course object from the given parameters.
+   * 
+   * @param the_course_title The Department and Code for the course
+   * @param the_course_description The full name of the course
+   * @param the_credit The credit worth of the course
+   */
+  public CourseCopy(String the_course_title, String the_course_description, int the_credit) {
+    super(the_course_title, the_course_description, the_credit);
+    my_teacher = NO_TEACHER_ASSIGNED_MESSAGE;
+    my_time = 0;
   }
+
+  /**
+   * @return The course time.
+   */
+  public int getTime() {
+    return my_time;
+  }
+
 
 }
