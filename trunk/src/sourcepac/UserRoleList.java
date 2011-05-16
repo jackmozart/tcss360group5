37ff21fd86703286b273710a49bd92c2f747c879
phillip.bernard1@gmail.com <phillip.bernard1@gmail.com@338336da-c548-6115-24be-3890e3b90027>
2011-05-16 03:21:06 +0000

    added more test methods, and checkstyle and formaters from 305 minus the final parameter enforcement
    --Phillip

---
+++ trunk/src/sourcepac/UserRoleList.java
@@ -0,0 +1,17 @@
+
+package sourcepac;
+
+import java.util.List;
+
+import users.Voter;
+
+/**
+ * This Class manages user roles for a specific user.
+ * 
+ * @author
+ * @version 5/13/2011
+ */
+public class UserRoleList {
+  List<Voter> my_roles;
+
+}
--- trunk/src/tests/ScheduleMainTest.java
+++ trunk/src/tests/ScheduleMainTest.java
@@ -1,21 +1,39 @@
+
 package tests;
+
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import sourcepac.ScheduleMain;
+import users.Advisor;
+import users.Student;
 
 public class ScheduleMainTest {
-	private ScheduleMain my_scheduler;
-	
-	@Before
-	public void startUp() {
-		my_scheduler = new ScheduleMain() ;
-	}
-	
-	@Test
-	public void loadFileTest() {
-		
-	}
-	
+  private ScheduleMain my_scheduler;
+
+  @Before
+  public final void startUp() {
+    my_scheduler = new ScheduleMain();
+  }
+
+  @Test
+  public void loadUserFileAdvisorTest() {
+    Advisor test_advisor = new Advisor("jsnuffy", "123456789", "Joe Snuffy", null);
+    my_scheduler.loadUsers("SingleAdvisorTest.txt");
+    assertEquals("Users should be the same", test_advisor, my_scheduler.getUser("jsnuffy"));
+
+  }
+
+  @Test
+  public void loadUserFileStudentTest() {
+    Student test_student = new Student("jsnuffy", "123456789", "Joe Snuffy", null, null);
+    my_scheduler.loadUsers("SingleStudentTest.txt");
+    assertEquals("Users should be the same", test_student, my_scheduler.getUser("jsnuffy"));
+  }
+
+  public void loadUserFileTeacherTest() {
+
+  }
 }
