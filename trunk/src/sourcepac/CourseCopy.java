01132a519c7bc6949490c1ba822c0ba20116c4a3
fromtheofficeofthepresident@gmail.com <fromtheofficeofthepresident@gmail.com@338336da-c548-6115-24be-3890e3b90027>
2011-05-16 22:09:32 +0000

    Course copy complete

--- trunk/src/sourcepac/CourseCopy.java
+++ trunk/src/sourcepac/CourseCopy.java
@@ -1,8 +1,16 @@
 
 package sourcepac;
 
-public class CourseCopy extends Course{
+/**
+ * Course copy stores a inputed course assigned to a teacher and time.  
+ * @author Steven Cozart 
+ * @version 1.0 5/16/11
+ */
+public class CourseCopy extends Course {
 
+  /**
+   * Default message if no teacher is assigned. 
+   */
   private static final String NO_TEACHER_ASSIGNED_MESSAGE = "No Teacher assigned";
   
   /**
@@ -11,9 +19,19 @@ public class CourseCopy extends Course{
   private String my_teacher;
   
   /**
+   * Time of the start course.
+   */
+  private int my_start_time;
+  
+  /**
    * Time of the course.
    */
-  private int my_time;
+  private int my_end_time;
+  
+  /**
+   * Day of the course.
+   */
+  private int my_day;
   
   /**
    * This constructor creates a course object from the given parameters.
@@ -25,14 +43,76 @@ public class CourseCopy extends Course{
   public CourseCopy(String the_course_title, String the_course_description, int the_credit) {
     super(the_course_title, the_course_description, the_credit);
     my_teacher = NO_TEACHER_ASSIGNED_MESSAGE;
-    my_time = 0;
+    my_start_time = 0;
+    my_end_time = 0;
+    my_day = 0;
   }
 
   /**
-   * @return The course time.
+   * This constructor creates a course object from the given parameters.
+   * 
+   * @param the_course_title The Department and Code for the course
+   * @param the_course_description The full name of the course
+   * @param the_credit The credit worth of the course
+   * @param the_start The start time of the course
+   * @param the_end The end time of the course.
    */
-  public int getTime() {
-    return my_time;
+  public CourseCopy(String the_course_title, String the_course_description, int the_credit,
+                    int the_start, int the_end) {
+    super(the_course_title, the_course_description, the_credit);
+    my_teacher = NO_TEACHER_ASSIGNED_MESSAGE;
+    my_start_time = the_start;
+    my_end_time = the_end;
+    my_day = 0;
+  }
+
+  /**
+   * This constructor creates a course object from the given parameters.
+   * 
+   * @param the_course_title The Department and Code for the course
+   * @param the_course_description The full name of the course
+   * @param the_credit The credit worth of the course
+   * @param the_start The start time of the course
+   * @param the_end The end time of the course.
+   * @param the_teacher The name of the teacher. 
+   * @param the_day The day of the class. 
+   */
+  public CourseCopy(String the_course_title, String the_course_description, int the_credit,
+                    int the_start, int the_end, String the_teacher, int the_day) {
+    super(the_course_title, the_course_description, the_credit);
+    my_teacher = the_teacher;
+    my_start_time = the_start;
+    my_end_time = the_end;
+    my_day = the_day;
+  }
+
+
+  /**
+   * @return The course start time.
+   */
+  public int getStartTime() {
+    return my_start_time;
+  }
+  
+  /**
+   * @return The course end time.
+   */
+  public int getEndTime() {
+    return my_end_time;
+  }
+  
+  /**
+   * @return The teacher of the course.
+   */
+  public String getTeacher() {
+    return my_teacher;
+  }
+  
+  /**
+   * @return The day course will be taught.
+   */
+  public int getDay() {
+    return my_day;
   }
 
 
