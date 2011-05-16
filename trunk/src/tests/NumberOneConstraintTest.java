76df404f23ae2d74bc9f379eff3182163a5643ef
cdtomyn@gmail.com <cdtomyn@gmail.com@338336da-c548-6115-24be-3890e3b90027>
2011-05-16 23:24:49 +0000

    Formatted correctly.

--- trunk/src/tests/NumberOneConstraintTest.java
+++ trunk/src/tests/NumberOneConstraintTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
+import sourcepac.Constraints;
 import sourcepac.CourseCopy;
 
 /**
@@ -20,30 +21,31 @@ import sourcepac.CourseCopy;
  */
 
 public class NumberOneConstraintTest {
+ /**
+  * Start time at 8.
+  */
+  public static final int START_TIME_ONE = 8;
+
+ /**
+  * First teacher name.
+  */
+  public static final String TEACHER_NAME_ONE = "Tenenberg";
+
+ /**
+  * End time at 10.
+  */
+  public static final int END_TIME_ONE = 10;
+
   /**
    * A list of CourseCopy.
    */
   private List<CourseCopy> my_schedule_list_one;
-  
+
   /**
    * A list of CourseCopy.
    */
   private List<CourseCopy> my_schedule_list_two;
-  
-  /**
-   * Start time at 8.
-   */
-  public static final int START_TIME_ONE = 8;
-  
-  /**
-   * First teacher name.
-   */
-  public static final String TEACHER_NAME_ONE = "Tenenberg";
-  
-  /**
-   * End time at 10.
-   */
-  public static final int END_TIME_ONE = 10;
+
 
   /**
    * Creates many CourseCopy to prepare for the testing.
@@ -52,29 +54,29 @@ public class NumberOneConstraintTest {
   public void setUp() {
     my_schedule_list_one = new ArrayList<CourseCopy>();
     my_schedule_list_one.add(new CourseCopy("TCSS 343", "A TCSS class", 5,
-    		                                START_TIME_ONE, END_TIME_ONE, TEACHER_NAME_ONE, 1));
+                         START_TIME_ONE, END_TIME_ONE, TEACHER_NAME_ONE, 1));
     my_schedule_list_one.add(new CourseCopy("TCSS 360", "A TCSS class", 5,
-    		                                START_TIME_ONE, END_TIME_ONE, TEACHER_NAME_ONE, 1));
+                         START_TIME_ONE, END_TIME_ONE, TEACHER_NAME_ONE, 1));
     my_schedule_list_two = new ArrayList<CourseCopy>();
     my_schedule_list_two.add(new CourseCopy("TCSS 343", "A TCSS class", 5,
-    		                                START_TIME_ONE, END_TIME_ONE, TEACHER_NAME_ONE, 1));
+                         START_TIME_ONE, END_TIME_ONE, TEACHER_NAME_ONE, 1));
     my_schedule_list_two.add(new CourseCopy("TCSS 360", "A TCSS class", 5,
-    		                                10, 12, TEACHER_NAME_ONE, 1));
+                         10, 12, TEACHER_NAME_ONE, 1));
   }
 
   /**
    * Checks to see if any teachers are teaching at the same time on the same
-   * day.
+   * day when the times are same.
    */
   @Test
   public void teacherSameTimeTestShouldFail() {
     for (int i = 0; i < my_schedule_list_one.size(); i++) {
       for (int j = i + 1; j < my_schedule_list_one.size(); j++) {
         if (my_schedule_list_one.get(i).getTeacher().equals
-          (my_schedule_list_one.get(j).getTeacher())) {
+           (my_schedule_list_one.get(j).getTeacher())) {
           if (my_schedule_list_one.get(i).getStartTime() ==
-        		my_schedule_list_one.get(j).getStartTime() &&
-                my_schedule_list_two.get(i).getDay() ==
+              my_schedule_list_one.get(j).getStartTime() &&
+              my_schedule_list_two.get(i).getDay() ==
                     my_schedule_list_two.get(j).getDay()) {
             fail("The teacher (teacher name here) is teaching" +
                 " two classes at the same time on the same day.");
@@ -83,13 +85,13 @@ public class NumberOneConstraintTest {
       }
     }
   }
+  
   /**
    * Checks to see if any teachers are teaching at the same time on the same
-   * day.
+   * day when the times are different.
    */
   @Test
   public void teacherSameTimeTestShouldSuccess() {
-    
     for (int i = 0; i < my_schedule_list_two.size(); i++) {
       for (int j = i + 1; j < my_schedule_list_two.size(); j++) {
         if (my_schedule_list_two.get(i).getTeacher()
@@ -97,12 +99,23 @@ public class NumberOneConstraintTest {
           if (my_schedule_list_two.get(i).getStartTime() ==
         	  my_schedule_list_two.get(j).getStartTime() &&
         	  my_schedule_list_two.get(i).getDay() ==
-                  my_schedule_list_two.get(j).getDay()) {
+              my_schedule_list_two.get(j).getDay()) {
             fail("The teacher " + my_schedule_list_two.get(i).getTeacher() +
             		" is teaching two classes at the same time on the same day.");
           }
         }
       }
     }
+  }
+  /**
+   * Checks the constraints class to see if it actually checks to see if the
+   * teacher doesn't teach at the same time on the same day.
+   */
+  @Test
+  public void teacherSameTimeConstraintTest() {
+    final Constraints constraint = new Constraints(my_schedule_list_one, null,
+			  null, null);
+    assertFalse(("Teacher " + "" + "is teaching two classes at the same time").equals
+      (constraint.teacherSameTimes()));
   }
 }
