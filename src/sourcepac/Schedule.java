09c7116d904866cce54cf0e861e5ee21e8e03ee3
windmaster.kaze <windmaster.kaze@gmail.com>
2011-05-16 21:17:21 +0000

    Added tests for scheduleGenerator's I/O functionality and proper course parsing

--- src/sourcepac/Schedule.java
+++ src/sourcepac/Schedule.java
@@ -1,6 +1,18 @@
 
 package sourcepac;
 
+import java.util.List;
+
 public class Schedule {
+
+  public void addCourse(CourseCopy courseCopy) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public List<CourseCopy> getCourses() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
---
+++ src/sourcepac/ScheduleGenerator.java
@@ -0,0 +1,20 @@
+package sourcepac;
+
+/**
+ * The ScheduleGenerator class
+ * @author Chris Davidson
+ * @version 5/16/2011
+ *
+ */
+public class ScheduleGenerator {
+
+  public void importSchedule(String string) {
+    // TODO Auto-generated method stub 
+  }
+
+  public Schedule getSchedule() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
