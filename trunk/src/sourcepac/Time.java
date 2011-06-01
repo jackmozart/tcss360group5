1f9ce3557dcc3a77e670658bc530f265cd3bb31d
fromtheofficeofthepresident@gmail.com <fromtheofficeofthepresident@gmail.com@338336da-c548-6115-24be-3890e3b90027>
2011-06-01 22:40:49 +0000

    added coments

--- trunk/src/sourcepac/Time.java
+++ trunk/src/sourcepac/Time.java
@@ -1,27 +1,51 @@
 
 package sourcepac;
 /**
- * Will add comments later need sleep
+ * Holds a single time block consisting of a start and end time. 
+ * @author Steven Cozart comments 
  * @author Phillip Bernard;
  * @version 1.0 5/21/2011
  */
 public class Time implements Comparable{
+  /**
+   * The start time of the block.
+   */
   private final int my_start_time;
+  
+  /**
+   * The end time of the block.
+   */
   private final int my_end_time;
   
+  /***
+   * 
+   * @param the_start_time The beginning time of the time block.
+   * @param the_end_time The ending time of the time block.  
+   */
   public Time(int the_start_time, int the_end_time) {
     my_start_time = the_start_time;
     my_end_time = the_end_time;
   }
   
+  /**
+   * 
+   * @return The beginning time of the time block.
+   */
   public int getStartTime() {
     return my_start_time;
   }
   
+  /**
+   * 
+   * @return The ending time of the time block
+   */
   public int getEndTime() {
     return my_end_time;
   }
 
+  /**
+   * @return -1 if time is before the given time, 0 if equal, and 1 if after.  
+   */
   @Override
   public int compareTo(Object other_object) {
     int result = 1;
